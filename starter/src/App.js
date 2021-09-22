f306a1b5edc24e36c1cf3aa759b227aaa8de989b
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-09-22 19:21:22 -0300

    starter: wrapper-cards

--- starter/src/App.js
+++ starter/src/App.js
@@ -14,6 +14,70 @@ const App = () => {
           </div>
         </div>
       </header>
+
+      <div className="container">
+        <section className="wrapper-cards">
+          <article className="card">
+            <img src="https://images.unsplash.com/photo-1585643248637-7d04f9461dd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1341&q=80" /> 
+
+            <div className="card-body">
+              <h2>Macbook Air 13" Apple M1</h2>
+              <p>R$ 8.725,94</p>
+              <a href="#">Comprar</a>  
+            </div> 
+          </article>
+
+          <article className="card">
+            <img src="https://images.unsplash.com/photo-1585643248637-7d04f9461dd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1341&q=80" /> 
+
+            <div className="card-body">
+              <h2>Macbook Air 13" Apple M1</h2>
+              <p>R$ 8.725,94</p>
+              <a href="#">Comprar</a>  
+            </div> 
+          </article>
+
+          <article className="card">
+            <img src="https://images.unsplash.com/photo-1585643248637-7d04f9461dd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1341&q=80" /> 
+
+            <div className="card-body">
+              <h2>Macbook Air 13" Apple M1</h2>
+              <p>R$ 8.725,94</p>
+              <a href="#">Comprar</a>  
+            </div> 
+          </article>  
+
+          <article className="card">
+            <img src="https://images.unsplash.com/photo-1585643248637-7d04f9461dd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1341&q=80" /> 
+
+            <div className="card-body">
+              <h2>Macbook Air 13" Apple M1</h2>
+              <p>R$ 8.725,94</p>
+              <a href="#">Comprar</a>  
+            </div> 
+          </article>  
+
+                  <article className="card">
+            <img src="https://images.unsplash.com/photo-1585643248637-7d04f9461dd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1341&q=80" /> 
+
+            <div className="card-body">
+              <h2>Macbook Air 13" Apple M1</h2>
+              <p>R$ 8.725,94</p>
+              <a href="#">Comprar</a>  
+            </div> 
+          </article>  
+
+          <article className="card">
+            <img src="https://images.unsplash.com/photo-1585643248637-7d04f9461dd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1341&q=80" /> 
+
+            <div className="card-body">
+              <h2>Macbook Air 13" Apple M1</h2>
+              <p>R$ 8.725,94</p>
+              <a href="#">Comprar</a>  
+            </div> 
+          </article>                        
+        </section>
+      </div>
     </>
   );
 }
