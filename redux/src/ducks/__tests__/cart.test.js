fe2cbb3e11bf62caadf58dfc0d6eb486ab49f679
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-10-25 14:54:31 -0300

    redux: cart tests remove_item

--- redux/src/ducks/__tests__/cart.test.js
+++ redux/src/ducks/__tests__/cart.test.js
@@ -1,25 +1,37 @@
 import { cart } from '../cart';
 import { Types } from '../../ducks/cart';
 
+const product = {
+  image:
+    'https://images-americanas.b2w.io/produtos/01/00/img/2018999/7/2018999764_3SZ.jpg',
+  price: 250.89,
+  title: 'Funko Pop The Office Dwight Schrute',
+  _id: '2',
+};
+
 describe('cart reducer', () => {
   it('should cart be a function', () => {
     expect(typeof cart).toBe('function');
   });
 
   it('should add a new product', () => {
-    const product = {
-      image:
-        'https://images-americanas.b2w.io/produtos/01/00/img/2018999/7/2018999764_3SZ.jpg',
-      price: 250.89,
-      title: 'Funko Pop The Office Dwight Schrute',
-      _id: '2',
-    };
     const initialState = {};
     const action = {
       type: Types.ADD_ITEM,
       payload: product,
     };
     const after = { 2: { ...product, amount: 1 } };
+
+    expect(cart(initialState, action)).toEqual(after);
+  });
+
+  it('should remove the product entered by id', () => {
+    const initialState = { 2: { ...product, amount: 1 } };
+    const action = {
+      type: Types.REMOVE_ITEM,
+      payload: '2',
+    };
+    const after = {};
 
     expect(cart(initialState, action)).toEqual(after);
   });
