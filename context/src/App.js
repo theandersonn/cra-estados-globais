66dc3a276a6e0741abe7126a56aaada151215211
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-10-27 21:15:03 -0300

    context: estrutura inicial

---
+++ context/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
