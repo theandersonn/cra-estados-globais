1c5474e31f4c3782ac9720bcd617ede8d3909a1e
theandersonn <theandersonn@gmail.com>
2022-06-23 14:57:15 -0300

    redux: ajusta alerta suporte reactDOM

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import store from './store';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
